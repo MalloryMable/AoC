7b3bc54edef7e420b56274323e80f5134f4d5115
Mallory Mable <mmcrumbliss@gmail.com>
2022-06-16 00:00:40 -0400

    Add files via upload

---
+++ 2015/Day 8/AoC_2015_8_1.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class AoC_2015_8_1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = args.length != 0 ? new File(args[0]) : new File("file.txt");
+        Scanner scanner = new Scanner(file);
+        int goodSum = 0, escapedSum = 0;
+        while(scanner.hasNext()){
+            String line = scanner.next();
+            escapedSum += line.length();
+            line = line.substring(1, line.length()-1);
+
+            for(int letter = 0; letter < line.length(); letter++){
+                if(line.charAt(letter) == '\\'){
+                    if(line.charAt(letter + 1) == 'x') {
+                        line = line.substring(0,letter) + " " + line.substring(letter + 4);
+
+                    } else {
+                        line = line.substring(0,letter) + " " + line.substring(letter + 2);
+
+                    }
+                    letter--;
+                }
+            }
+            goodSum += line.length();
+        }
+        scanner.close();
+
+        System.out.printf("Good sum %d - %d: %d",goodSum, escapedSum,  escapedSum - goodSum);
+    }
+}
