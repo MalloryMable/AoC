b6fe03173fb1256f5709a3c42984f7aa72d43a2e
Mallory Mable <mallorymable@proton.me>
2023-12-09 09:35:24 -0500

    more binaries. Get outta here

--- 2023/day-09/pt-1/target/debug/aoc-05-01.d
+++
@@ -1,1 +0,0 @@
-/home/mallory/git-repos/AoC/2023/day-09/pt-1/target/debug/aoc-05-01: /home/mallory/git-repos/AoC/2023/day-09/pt-1/src/main.rs
--- 2023/day-09/pt-1/target/debug/deps/aoc_05_01-9aa48ae75d5f7765.d
+++
@@ -1,5 +0,0 @@
-/home/mallory/git-repos/AoC/2023/day-09/pt-1/target/debug/deps/aoc_05_01-9aa48ae75d5f7765: src/main.rs
-
-/home/mallory/git-repos/AoC/2023/day-09/pt-1/target/debug/deps/aoc_05_01-9aa48ae75d5f7765.d: src/main.rs
-
-src/main.rs:
