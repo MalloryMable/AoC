deaa162a991e28be739e002167f3101abfbb7b45
Mallory Mable <mallorymable@proton.me>
2024-12-03 00:31:06 -0500

    saving day 3 work before bed

--- 2024/day-03/pt-1/src/main.rs
+++ 2024/day-03/pt-1/src/main.rs
@@ -18,13 +18,14 @@ fn reader_from_path(relative_path: &str) -> BufReader<File> {
 fn main() {
     let input_file = env::args()
         .nth(1)
-        // could ok_or_else and propogate an error but as process is killed type change is avoided
         .unwrap_or_else( || { 
             eprintln!("Missing input file");
             std::process::exit(1); 
         });
 
     let reader = reader_from_path(&input_file);
+
+    // regex mul\((\d+),(\d+)\) while has next, extract matches, parse to int, multiply, sum
 
     for line in reader.lines().find_map(Result::ok) {
         println!("{}", line);
